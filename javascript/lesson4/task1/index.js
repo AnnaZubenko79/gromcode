719e8a01cc50f8facc68d9edb6b35949f842e220
AnnaZubenko79 <annazubenko79@gmail.com>
2022-05-27 18:30:47 +0300

    created eslint installed node.js solve task4 in lesson 4

---
+++ javascript/lesson4/task1/index.js
@@ -0,0 +1,6 @@
+const n = 101;
+sum = 0;
+for (let i = 1; i <= n; i++) {
+  sum += i;
+}
+console.log('Result:', sum);
---
+++ javascript/lesson4/task3/index.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 5; i++) {
+  console.log(`=> ${i} <=`);
+  for (let k = 1; k <= 10; k++) {
+    console.log(`${i} x ${k} = ${k * i}`);
+  }
+}

1f57e0b55fc74dfb98c70ad2118cb31d892ec138
AnnaZubenko79 <annazubenko79@gmail.com>
2022-07-01 18:06:53 +0300

    fixed naming

--- javascript/lesson13/task2/calculator.test.js
+++ javascript/lesson13/task2/calculator.test.js
@@ -1,7 +1,7 @@
-import getSum, { getSquareArray, getOddNumbers } from './calculator.js';
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
 
 it('should get squared numbers', () => {
-  const result = getSquareArray([1, 2, 3]);
+  const result = getSquaredArray([1, 2, 3]);
   expect(result).toEqual([1, 4, 9]);
 });
 it('should keep odd numbers only', () => {

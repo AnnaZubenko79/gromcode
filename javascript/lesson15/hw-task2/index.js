e9f25026ad771aeb92b6aa75ccdd79b3887d8a2c
AnnaZubenko79 <annazubenko79@gmail.com>
2022-07-29 17:38:48 +0300

    lesson 34 created

--- javascript/lesson15/hw-task2/index.js
+++ javascript/lesson15/hw-task2/index.js
@@ -1,63 +1,100 @@
-export const createLogger = () => {
+// export const createLogger = () => {
+//   const memory = [];
+//   function getRecords(type) {
+//     if (type === undefined) {
+//       return memory.sort((a, b) => b.dateTime - a.dateTime);
+//     }
+//     return memory.filter(elem => elem.type === type).sort((a, b) => b.dateTime - a.dateTime);
+//   }
+//   function warn(message) {
+//     const memoryMessage = {
+//       message: `${message}`,
+//       type: 'warn',
+//       dateTime: new Date(),
+//     };
+//     memory.push(memoryMessage);
+//     return memory;
+//   }
+//   function error(message) {
+//     const memoryMessage = {
+//       message: `${message}`,
+//       type: 'error',
+//       dateTime: new Date(),
+//     };
+//     memory.push(memoryMessage);
+//     return memory;
+//   }
+//   function log(message) {
+//     const memoryMessage = {
+//       message: `${message}`,
+//       type: 'log',
+//       dateTime: new Date(),
+//     };
+//     memory.push(memoryMessage);
+//     return memory;
+//   }
+//   return {
+//     warn,
+//     error,
+//     log,
+//     getRecords,
+//   };
+// };
+
+// const logger1 = createLogger();
+// console.log(logger1.log('User logged in'));
+// console.log(logger1.warn('User is tring to ented restricted page'));
+// console.log(logger1.log('User logged out'));
+// console.log(logger1.error('Unexpected error on the site'));
+
+// console.log(logger1.getRecords()); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+// console.log(logger1.getRecords('log')); // ===> [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
+// console.log(logger1.getRecords('error')); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }]
+// console.log(logger1.getRecords('warn')); // ===> [{ message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }]
+
+// // const logger2 = createLogger();
+// console.log(logger2.warn('Opps, something is happenning'));
+// console.log(logger2.getRecords('error')); // ===> []
+// console.log(logger2.getRecords('warn')); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
+// console.log(logger2.getRecords()); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
+
+// const logger3 = createLogger();
+// console.log(logger3.getRecords('error')); // ===> []
+// console.log(logger3.getRecords()); // ===> []
+
+const createLogger = () => {
   const memory = [];
-  function getRecords(type) {
-    if (type === undefined) {
-      return memory.sort((a, b) => b.dateTime - a.dateTime);
-    }
-    return memory.filter(elem => elem.type === type).sort((a, b) => b.dateTime - a.dateTime);
-  }
-  function warn(message) {
-    const memoryMessage = {
-      message: `${message}`,
-      type: 'warn',
-      dateTime: new Date(),
+  const pushArr = (text, type) => {
+    const memArr = {
+      message: text,
+      date: new Date(),
+      type: type,
     };
-    memory.push(memoryMessage);
+    memory.push(memArr);
     return memory;
-  }
-  function error(message) {
-    const memoryMessage = {
-      message: `${message}`,
-      type: 'error',
-      dateTime: new Date(),
-    };
-    memory.push(memoryMessage);
+  };
+  const getRecords = () => {};
+  const warn = text => {
+    pushArr(text, 'warn');
     return memory;
-  }
-  function log(message) {
-    const memoryMessage = {
-      message: `${message}`,
-      type: 'log',
-      dateTime: new Date(),
-    };
-    memory.push(memoryMessage);
+  };
+  const error = text => {
+    pushArr(text, 'error');
     return memory;
-  }
+  };
+  const log = text => {
+    pushArr(text, 'log');
+    return memory;
+  };
   return {
+    getRecords,
     warn,
     error,
     log,
-    getRecords,
   };
 };
-
 const logger1 = createLogger();
 console.log(logger1.log('User logged in'));
 console.log(logger1.warn('User is tring to ented restricted page'));
 console.log(logger1.log('User logged out'));
 console.log(logger1.error('Unexpected error on the site'));
-
-console.log(logger1.getRecords()); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
-console.log(logger1.getRecords('log')); // ===> [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
-console.log(logger1.getRecords('error')); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }]
-console.log(logger1.getRecords('warn')); // ===> [{ message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }]
-
-const logger2 = createLogger();
-console.log(logger2.warn('Opps, something is happenning'));
-console.log(logger2.getRecords('error')); // ===> []
-console.log(logger2.getRecords('warn')); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
-console.log(logger2.getRecords()); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
-
-const logger3 = createLogger();
-console.log(logger3.getRecords('error')); // ===> []
-console.log(logger3.getRecords()); // ===> []
--- javascript/lesson31/task1/index.js
+++ javascript/lesson31/task1/index.js
@@ -18,16 +18,21 @@ export const requestUserData = userId => {
   return request;
 };
 
-requestUserData('user777')
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+// requestUserData('user777')
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
 
-//   const req = requestUserData('broken')
-//     .then(data => (data = 5))
-//     .catch(error => {
-//       console.log(error);
-//       throw new Error('Error');
-//     })
-//     .then(value => value * 2);
-//   //   .catch(error => console.log(error));
-//   req.then(data => console.log(data));
+// const req = requestUserData('broken');
+// req
+//   .then(data => {
+//     data = 5;
+//     console.log(data);
+//     return data;
+//   })
+//   .catch(error => {
+//     console.log(error);
+//     throw new Error('Error');
+//   })
+//   .then(value => console.log(value * 2))
+//   .catch(error => console.log(error));
+// // req.then(data => console.log(data));
